efa46252cdf45a4f757f5c64933aecb89fe32e59
aege06 <aysegulkucuk06006@gmail.com>
2022-01-24 09:39:08 -0600

    Initial work is done

---
+++ src/hello/Hello.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World ");
+        System.out.println("B25 is learning Git and GitHub");
+
+    }
+}
